e56e3431ab604c102871568934108288f57e4f66
keerthi-rkl <keerthiagustd.p@gmail.com>
2021-12-14 18:20:22 +0530

    bts

---
+++ arrays/one.js
@@ -0,0 +1,8 @@
+let emp=["rakesh","harry","suga","jimin"]
+console.log(emp[1]);
+console.log(emp[3])
+for(let i=0;i<=emp.length-1;i++){
+    console.log(emp[i])
+}
+console.log(emp.length);
+console.log(emp.slice());
