0d44493b131bc4404a91204ef34b359ffbeeca34
keerthi-rkl <keerthiagustd.p@gmail.com>
2021-12-20 23:50:01 +0530

    commit

---
+++ jsevents/script.js
@@ -0,0 +1,15 @@
+function wish(){
+    alert("hello i am like suga")
+}
+function over(){
+   document.getElementById("but1").setAttribute("style","background-color:red");
+}
+
+function on(){
+    document.getElementById("but2").setAttribute("style","background-color:blue");
+
+}
+function out(){
+    document.getElementById("but3").setAttribute("style","background-color:pink");
+
+}
