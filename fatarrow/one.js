07f588521c42136835b387bee5dff698608c58aa
keerthi-rkl <keerthiagustd.p@gmail.com>
2021-12-21 22:36:03 +0530

    commit

---
+++ fatarrow/one.js
@@ -0,0 +1,21 @@
+/* let msg=(a)=>{
+    console.log( "good morning have a nice day")
+}
+msg(); */
+
+/* 
+let multi=(a,b)=>{
+    console.log(a*b*2)
+}
+multi(3,3) */
+
+/* let add=(a,b,c)=>{
+    return a+b+c-10;
+}
+let res=add(34,2,1);
+console.log(res) */
+  
+
+let add=(a,b)=>a+b;
+let res=add(1,2)
+console.log(res)
