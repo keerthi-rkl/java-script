7439c59b3919a86af2180aef6076a00a84bde73d
keerthi-rkl <keerthiagustd.p@gmail.com>
2022-01-08 18:03:43 +0530

    commmit

---
+++ bcrypt/student.js
@@ -0,0 +1,11 @@
+const bcrypt=require("bcryptjs")
+let student={name:"rakesh",
+             id:34,
+             course:"cumputer science engineering",
+             email:"pulirakesh@gmail.com",
+             password:"123bts$"}
+let salt=bcrypt.genSaltSync(3)
+let new_password=bcrypt.hashSync(student.password,salt);
+console.log(new_password)
+ let new_student={...student,password:new_password}
+ console.log(new_student)
